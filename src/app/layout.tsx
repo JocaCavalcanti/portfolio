b34d594f74e85f6f637cfd84852f4623d4400694
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-02 14:34:03 -0300

    feat(project): :construction: importing the HeaderComponenet and adding the temporary logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css"
+import HeaderComponent from "@/components/header/headerComponent"
 
 
 export default function RootLayout({
@@ -6,7 +7,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body  className="bg-stone-900 min-h-dvh box-border name">
+        <div className="max-w-7xl mx-auto w-full px-4">
+          <HeaderComponent />
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
+
