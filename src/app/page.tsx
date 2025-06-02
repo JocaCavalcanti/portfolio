9370783d6442300a703ad22b3aebfe2661106e60
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-02 18:30:20 -0300

    feat(bentuUi): :bricks: adding BentoUiGrid on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import BentoUiGrid from '../section/bentoUi/bentoUiGrid';
 
 export default function HomePage() {
   return (
-    <main>
-
+    <main>  
+      <BentoUiGrid />
     </main>
   )
 }
