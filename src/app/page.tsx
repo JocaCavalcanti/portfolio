571f9c4ec60e65c0b569958f5f843a1a71e9bca4
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-07 11:58:28 -0300

    refactor(project): :bricks:  adjusting the size of the main website screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BentoUiGrid from '../section/bentoUi/bentoUiGrid';
 
 export default function HomePage() {
   return (
-    <main>  
+    <main className='min-h-screen'>  
       <BentoUiGrid />
     </main>
   )
