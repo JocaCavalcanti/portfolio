7e38f1cac2b5381bd0df788d6b070ff83c6e8375
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-08 09:51:26 -0300

    refactor(HomePage): :recycle: replacing BentoUiGrid with HeroPage and removing headerComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroPageComponent from '@/components/heroPage/heroPageComponent';
 import BentoUiGrid from '../section/bentoUi/bentoUiGrid';
 
 export default function HomePage() {
   return (
-    <main className='min-h-screen'>  
-      <BentoUiGrid />
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>  
+      <HeroPageComponent />
     </main>
   )
 }
