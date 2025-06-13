9fc0f4075eb50e71914b3d596797b6d6ba090976
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-12 21:13:49 -0300

    feat(HomePage): :bricks: adding aboutComponent in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import HeroPageComponent from '@/components/heroPage/heroPageComponent';
-import BentoUiGrid from '../section/bentoUi/bentoUiGrid';
+import HeroPageComponent from '@/components/heroPage/heroPageComponent'
+import AboutComponent from '@/components/about/aboutComponent'
+
 
 export default function HomePage() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>  
-      <HeroPageComponent />
+     <HeroPageComponent />
+     <AboutComponent />
     </main>
   )
 }
