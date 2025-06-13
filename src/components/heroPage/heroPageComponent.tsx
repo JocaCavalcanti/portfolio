483bf258fb9a04d34e6f6e7fb35c114b4cc529e0
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-12 21:11:52 -0300

    feat(HeroPageComponent): :recycle: removing parts of the code that I will no longer use and leaving the heroPage mocked and ready for future interventions

--- src/components/heroPage/heroPageComponent.tsx
+++ src/components/heroPage/heroPageComponent.tsx
@@ -1,3 +1,5 @@
+
+
 'use client'
 
 import { useEffect, useRef, useState } from "react"
@@ -9,58 +11,10 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 
 
+
 gsap.registerPlugin(ScrollTrigger)
 
 export default function HeroPageComponent() {
-
- const [currentIndex, setCurrentIndex] = useState(1)
-  const [hasClickedd, setHasClicked] = useState(false)
-
-  const [isLoading, setisLoading] = useState(true)
-  const [loadedVideos, setLoadedVideos] = useState(0)
-
-  const totalVideos = 4
-  const nextVideoRef = useRef<HTMLVideoElement | null>(null)
-
-  const handleVideoLoad = () => {
-    setLoadedVideos((prev) => prev + 1)
-  }
-
-  const upcomingVideoIndex = (currentIndex % totalVideos) + 1
-
-  const handleMiniVideoClick = () => {
-    setHasClicked(true)
-
-    setCurrentIndex(upcomingVideoIndex)
-  }
-
-  useEffect(() => {
-    if (loadedVideos === totalVideos -1) {
-      setisLoading(false)
-    }
-  }, [loadedVideos])
-
-  useGSAP(() => {
-    if(hasClickedd){
-      gsap.set("#next-video", { visibility: "visible" })
-      gsap.to("#next-video", {
-        transformOrigin: "center center",
-        scale: 1,
-        width: "100%",
-        height: "100%",
-        duration: 1,
-        ease: "power1.inOut",
-        onStart: () => {
-        nextVideoRef.current?.play()},
-      })
-       gsap.from("#current-video", {
-          transformOrigin: "center center",
-          scale: 0,
-          duration: 1.5,
-          ease: "power1.inOut",
-        })
-    }
-  }, {dependencies: [currentIndex], revertOnUpdate: true})
 
   useGSAP(() => {
 
@@ -83,52 +37,14 @@ export default function HeroPageComponent() {
 
   })
 
-  const getVideoSrc = (index: any) => `videos/hero-${index}.mp4`
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      {/* {isLoading && (
-        <div>
-          <div className=" flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-            <div className="three-body">
-                <div className="three-body__dot" />
-                <div className="three-body__dot" />
-                <div className="three-body__dot" />
-            </div>
-          </div>
-        </div>
-      )} */}
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
-        <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVideoClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
-              <video 
-                ref={nextVideoRef}
-                src={getVideoSrc(upcomingVideoIndex) }
-                loop
-                muted
-                id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoad}
-              />
-            </div>
-          </div>
-          <video
-            ref={nextVideoRef}
-            src={getVideoSrc(currentIndex)}
-            loop
-            muted
-            id="next-video"
-            className="absolute-center invisible z-20 size-64 object-cover object-center"
-            onLoadedData={handleVideoLoad}
-          />
-          <video 
-          src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-          autoPlay
-          loop
-          muted
+        <div> 
+          <img 
+          src='/img/dark1.webp'
           className="absolute left-0 top-0 size-full object-cover object-center"
-          onLoadedData={handleVideoLoad}
           />
           <h1 className=" text-blue-75 hero-heading absolute bottom-5 right-5 z-40 ">G<b>a</b>ming</h1>
           <div className="absolute left-0 top-0 z-40 size-full">
