ab42dd759bce61b6711fb9ea4092adf2aca2f772
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-17 16:57:40 -0300

    feat(AboutComponent): :bricks: building the AboutComponent structure
    I have already brought the componentization of BentoCard

--- src/components/about/aboutComponent.tsx
+++ src/components/about/aboutComponent.tsx
@@ -1,10 +1,27 @@
-
+import BentoCard from '@/components/bentoCard/bentoCardComponent'
 
 
 export default function AboutComponent() {
     return (
-        <div>
-
-        </div>
+        <section className="bg-black pb-52">
+          <div className="container mx-auto px-3 md:px-10">
+            <div className="px-5 py-32">
+              <p className="font-main text-lg text-blue-50">
+                Lorem Title
+              </p>
+            <p className="font-main max-w-md text-lg text-blue-50 opacity-50">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt delectus inventore eius doloremque perferendis fuga error! Omnis, fugit. Voluptatum vitae modi eligendi magni doloribus commodi ducimus harum non sapiente sit!
+            </p>
+            </div>
+          
+            <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+              <BentoCard 
+                src="videos/feature-1.mp4"
+                title={<>radi<b>O</b></>}
+                description="lorem"
+              />
+            </div>
+          </div>
+        </section>
     )
 }
