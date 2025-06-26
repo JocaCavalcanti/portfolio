850c04388e4835f53902bab1098d39410155e187
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2025-06-25 22:09:16 -0300

    feat(BentoCardComponent): :lipstick: Adding text styling to the Bento Card Component
    And capitalizing the component name

--- src/components/bentoCardTilt/bentoCardComponent.tsx
+++ src/components/bentoCardTilt/bentoCardComponent.tsx
@@ -7,14 +7,14 @@ type Props = {
   description?: string
 }
 
-export default function bentoCardComponent({ src, title, description }: Props){
+export default function BentoCardComponent({ src, title, description }: Props){
 
   return (
     <div className='relative size-full'>
       <video src={src} loop muted autoPlay className='absolute left-0 top-0 size-full object-cover object-center'></video>
       <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
         <div>
-          <h1 className='bento-title font-main'>{title}</h1>
+          <h1 className='uppercase md:text-6xl text-4xl font-black font-main'>{title}</h1>
           {description &&(
             <p className='mt-3 max-w-64 text-xs md:text-base'>{description}</p>
           )}
